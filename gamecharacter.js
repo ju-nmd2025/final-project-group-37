0cd5b70fff300bb04d3989950086823f7ab55e3a
Deimaa <adde25my@student.ju.se>
2025-12-03 22:53:33 +0100

    Started drawing the character

--- gamecharacter.js
+++ gamecharacter.js
@@ -8,7 +8,20 @@ export default class Character {
   }
 
   draw() {
-    rect(this.x, this.y, this.w, this.h);
+    strokeWeight(3);
+    fill(100, 90, 200, 250);
+    stroke(100, 70, 200, 250);
+    circle(this.x + this.w / 2, this.y + this.h * 0.7, this.w * 0.75);
+
+    strokeWeight(3);
+    fill(80, 70, 200, 250);
+    stroke(80, 50, 200, 250);
+    circle(this.x + this.w * 0.25, this.y + this.h * 0.2, this.w * 0.35);
+
+    strokeWeight(3);
+    fill(200, 20, 200, 500);
+    stroke(70, 40, 200, 250);
+    circle(this.x + this.w * 0.25, this, y + this.h * 0.2, this.w * 0.2);
   }
 
   isColliding(character, platform) {

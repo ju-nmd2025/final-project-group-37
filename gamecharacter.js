f6267e26b99869cec58818f2ffe60da2a8525621
Deimaa <adde25my@student.ju.se>
2025-12-05 21:43:22 +0100

    Finished the base of the character

--- gamecharacter.js
+++ gamecharacter.js
@@ -22,6 +22,16 @@ export default class Character {
     fill(200, 20, 200, 500);
     stroke(70, 40, 200, 250);
     circle(this.x + this.w * 0.25, this, y + this.h * 0.2, this.w * 0.2);
+
+    strokeWeight(3);
+    fill(80, 70, 200, 250);
+    stroke(80, 50, 200, 250);
+    circle(this.x + this.w * 0.75, this.y + this.h * 0.2, this.w * 0.35);
+
+    strokeWeight(3);
+    fill(200, 20, 200, 500);
+    stroke(70, 40, 200, 250);
+    circle(this.x + this.w * 0.75, this.y + this.h * 0.2, this.w * 0.2);
   }
 
   isColliding(character, platform) {

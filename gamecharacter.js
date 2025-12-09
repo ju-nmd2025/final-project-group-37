e11ebe73bfd94251803a675c87d67387e54cf43a
Deimaa <adde25my@student.ju.se>
2025-12-09 16:22:37 +0100

    Added mouth, blush and eyelids

--- gamecharacter.js
+++ gamecharacter.js
@@ -50,6 +50,37 @@ export default class Character {
     fill(10, 10, 10, 250);
     noStroke();
     ellipse(this.x + 230, this.y + 130, 20, 30); // right pupil
+
+    strokeWeight(3);
+    fill(80, 70, 200, 250);
+    stroke(70, 40, 200, 250);
+    arc(this.x + 200, this.y + 170, 60, 50, 0, PI); // mouth
+
+    fill(200, 20, 200, 250);
+    noStroke();
+    circle(this.x + 150, this.y + 170, 20);
+
+    fill(200, 20, 200, 250);
+    noStroke();
+    circle(this.x + 250, this.y + 170, 20);
+
+    // blush
+
+    push();
+    strokeWeight(3);
+    fill(80, 70, 200, 250);
+    stroke(70, 40, 200, 250);
+    arc(this.x + 170, this.y + 130, 30, 50, PI, TWO_PI);
+    pop();
+
+    push();
+    strokeWeight(3);
+    fill(80, 70, 200, 250);
+    stroke(70, 40, 200, 250);
+    arc(this.x + 230, this.y + 130, 30, 50, PI, TWO_PI);
+    pop();
+
+    // eyelids
   }
 
   isColliding(character, platform) {

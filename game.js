cfc466f6e2d7fcfaf1fc8542ed204146ae053953
Deimaa <adde25my@student.ju.se>
2025-12-16 22:15:57 +0100

    Fixed the score count and restored the breaking platforms

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ import Character from "./character";
 // Game state management
 let gameState = 0; // 0: Start, 1: Playing, 2: Game Over
 let score = 0; // starts counting the points from 0
-let scoredPlatforms = 0;
+let lastPlatform = null;
 // Button drawing and interaction
 function drawButton(x, y, w, h, label) {
   fill(100, 200, 100);
@@ -104,7 +104,16 @@ function draw() {
     let platform = activePlatforms[i];
     if (character.vy >= 0 && character.isColliding(platform)) {
       character.isOnPlatform = true;
-      character.jump();
+
+      if (platform !== lastPlatform) {
+        score++;
+        lastPlatform = platform;
+      }
+
+      if (platform instanceof BreakingPlatform) {
+        platform.break();
+      }
+      if (platform) character.jump();
       character.y = platform.y - character.h;
       break;
     }
@@ -121,16 +130,6 @@ function draw() {
   }
 
   platformManager.update(scroll);
-
-  let activeCount = platformManager.getActivePlatforms().length;
-
-  if (scroll > 0) {
-    let removedPlatforms = scoredPlatforms - activeCount;
-    if (removedPlatforms > 0) {
-      score += removedPlatforms;
-    }
-  }
-  scoredPlatforms = activeCount;
 
   if (character.y > canvasHeight) {
     gameState = 2;
@@ -154,13 +153,13 @@ function mousePressed() {
     gameState = 1;
     character = new Character(50, 10, 50, 50);
     platformManager.init();
-    scoredPlatforms = platformManager.getActivePlatforms().length;
     score = 0;
+    lastPlatform = null;
   } else if (gameState === 2 && isMouseOnButton(125, 200, 150, 50)) {
     gameState = 1;
     character = new Character(50, 10, 50, 50);
     platformManager.init();
-    scoredPlatforms = platformManager.getActivePlatforms().length;
     score = 0;
+    lastPlatform = null;
   }
 }
